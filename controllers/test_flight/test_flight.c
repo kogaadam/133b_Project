18324bec693cb97ddf63532edb096b8d7a53cd52
kogaadam <43ada21@gmail.com>
2021-02-23 19:02:44 -0800

    added new controller for testing basic flight commands

---
+++ controllers/test_flight/test_flight.c
@@ -0,0 +1,69 @@
+/*
+ * File:          test_flight.c
+ * Date:
+ * Description:
+ * Author:
+ * Modifications:
+ */
+
+/*
+ * You may need to add include files like <webots/distance_sensor.h> or
+ * <webots/motor.h>, etc.
+ */
+#include <webots/robot.h>
+#include <webots/motor.h>
+
+
+/*
+ * You may want to add macros here.
+ */
+#define TIME_STEP 64
+
+/*
+ * This is the main program.
+ * The arguments of the main function can be specified by the
+ * "controllerArgs" field of the Robot node
+ */
+int main(int argc, char **argv) {
+  /* necessary to initialize webots stuff */
+  wb_robot_init();
+  int timestep = (int)wb_robot_get_basic_time_step();
+
+  // Get propeller motors and set them to velocity mode.
+  WbDeviceTag front_left_motor = wb_robot_get_device("front left propeller");
+  WbDeviceTag front_right_motor = wb_robot_get_device("front right propeller");
+  WbDeviceTag rear_left_motor = wb_robot_get_device("rear left propeller");
+  WbDeviceTag rear_right_motor = wb_robot_get_device("rear right propeller");
+  WbDeviceTag motors[4] = {front_left_motor, front_right_motor, rear_left_motor, rear_right_motor};
+  int m;
+  for (m = 0; m < 4; ++m) {
+    wb_motor_set_position(motors[m], INFINITY);
+    wb_motor_set_velocity(motors[m], 1.0);
+  }
+
+  /* main loop
+   * Perform simulation steps of TIME_STEP milliseconds
+   * and leave the loop when the simulation is over
+   */
+  while (wb_robot_step(TIME_STEP) != -1) {
+    /*
+     * Read the sensors :
+     * Enter here functions to read sensor data, like:
+     *  double val = wb_distance_sensor_get_value(my_sensor);
+     */
+
+    /* Process sensor data here */
+
+    /*
+     * Enter here functions to send actuator commands, like:
+     * wb_motor_set_position(my_actuator, 10.0);
+     */
+  };
+
+  /* Enter your cleanup code here */
+
+  /* This is necessary to cleanup webots resources */
+  wb_robot_cleanup();
+
+  return 0;
+}
